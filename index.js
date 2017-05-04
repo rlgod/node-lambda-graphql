f6dd8554d4076fb0af9432ef3382fed690cbd5e5
Daniel Parker <dparker.tech@gmail.com>
2017-05-04 18:47:34 +1000

    Add cors

--- index.js
+++ index.js
@@ -2,19 +2,21 @@
 const awsServerlessExpress = require('aws-serverless-express')
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
+const cors = require('cors');
 
 module.exports = (options) => {
 
   let app = express()
 
   app.use(graphqlHTTP(options))
+  app.use(cors());
 
   let server = awsServerlessExpress.createServer(app)
 
   return {
     handler: (event, context) => {
       return awsServerlessExpress.proxy(server, event, context)
-    },close: () => {
+    }, close: () => {
       server.close()
     }
   }
